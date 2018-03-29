9222fc2a9d82e6c8dc2e07e8c0ef6827a4996e4b
honpery <honpery@gmail.com>
2018-03-29 11:53:31 +0800

    add report gen api.

--- src/routes/report.ts
+++ src/routes/report.ts
@@ -2,9 +2,22 @@
  * report api
  */
 import * as Router from 'koa-router';
+import * as puppeteer from 'puppeteer';
 
 export const ReportRouter = new Router();
 
 ReportRouter.get('/reports', async ctx => {
-    ctx.body = 'pong';
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto('https://www.baidu.com', { waitUntil: 'networkidle2' });
+    const pdf = await page.pdf({ format: 'A4' });
+    await browser.close();
+
+    const filename = 'demo';
+
+    ctx.set('Content-disposition', 'attachment; filename=' + filename);
+    ctx.set('Content-type', 'application/pdf');
+
+    ctx.body = pdf;
 });
